5c1930a89b0e688b07eb94841f35c0805644684d
Yishai Rose <yishaijrose@gmail.com>
2024-02-06 23:12:26 +0200

    Completed homepage design

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const searchOpen = document.querySelector(".header__search-icon");
+const searchClose = document.querySelector(".search-form__close-icon");
+const search = document.querySelector(".search");
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-form__input");
+
+searchOpen.addEventListener("click", () => {
+  search.classList.toggle("search--visible");
+  searchInput.focus();
+});
+searchClose.addEventListener("click", () => {
+  searchForm.reset();
+  search.classList.toggle("search--visible");
+});
