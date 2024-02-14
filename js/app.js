64ab32eefcd4bda7ea43657e878542342658c394
Yishai Rose <yishaijrose@gmail.com>
2024-02-14 06:48:01 +0200

    completed UI

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const searchClose = document.querySelector(".search-form__close-icon");
 const search = document.querySelector(".search");
 const searchForm = document.querySelector(".search-form");
 const searchInput = document.querySelector(".search-form__input");
+const uploadBtn = document.querySelector(".header__upload-icon");
+const uploadOverlay = document.querySelector(".upload-modal");
+const closeOverlay = document.querySelector(".modal__close-btn");
 
 searchOpen.addEventListener("click", () => {
   search.classList.toggle("search--visible");
@@ -10,4 +13,11 @@ searchOpen.addEventListener("click", () => {
 searchClose.addEventListener("click", () => {
   searchForm.reset();
   search.classList.toggle("search--visible");
+});
+
+uploadBtn.addEventListener("click", () => {
+  uploadOverlay.classList.toggle("modal-visible");
+});
+closeOverlay.addEventListener("click", () => {
+  uploadOverlay.classList.toggle("modal-visible");
 });
